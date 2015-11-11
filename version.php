aaf64a69c74d9936715259621edd1031081195b2
Fred Woolard <woolardfa@appstate.edu>
2015-11-11 11:21:54 -0500

    Ver. 2013052007 Release 28-1.6 (Build: 2015111100)

--- version.php
+++ version.php
@@ -28,9 +28,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013052006;       // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012062500;       // Requires this Moodle version
+$plugin->version   = 2013052007;       // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2014111008.13;    // Requires this Moodle version
 $plugin->cron      = 0;
 $plugin->component = 'filter_rtmp';    // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.5 (Build: 2014102000)';
+$plugin->release   = '28-1.6 (Build: 2015111100)';
